cf7e1e1e166c2a495e94f1b4c44b7c94cc319194
StoneBlood <brkalix@gmail.com>
2025-06-16 18:18:19 +0300

    implemented the methods `findAll` and `getById`

--- src/main/java/job/scraper/service/JobService.java
+++ src/main/java/job/scraper/service/JobService.java
@@ -1,8 +1,14 @@
 package job.scraper.service;
 
+import job.scraper.dto.JobResponseDto;
 import job.scraper.model.Job;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface JobService {
-    void saveAll(List<Job> jobs);
+
+    Page<JobResponseDto> findAll(Pageable pageable);
+
+    JobResponseDto getById(Long id);
 }
--- src/main/java/job/scraper/service/JobServiceImpl.java
+++ src/main/java/job/scraper/service/JobServiceImpl.java
@@ -1,15 +1,32 @@
 package job.scraper.service;
 
+import job.scraper.dto.JobResponseDto;
+import job.scraper.exception.EntityNotFoundException;
+import job.scraper.mapper.JobMapper;
 import job.scraper.model.Job;
-import java.util.List;
+import job.scraper.repository.JobRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class JobServiceImpl implements JobService {
-    @Override
-    public void saveAll(List<Job> jobs) {
+    private final JobRepository jobRepository;
+    private final JobMapper jobMapper;
 
+    @Override
+    public Page<JobResponseDto> findAll(Pageable pageable) {
+        Page<Job> jobs = jobRepository.findAll(pageable);
+        return jobs.map(jobMapper::toDto);
+    }
+
+    @Override
+    public JobResponseDto getById(Long id) {
+        Job job = jobRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't fond a job with id: " + id)
+        );
+        return jobMapper.toDto(job);
     }
 }
